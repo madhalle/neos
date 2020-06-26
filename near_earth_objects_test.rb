76f8fdb5f188f1bb867b9b2ba822df1078a1fcbd
Mad Hallé <mad.halle@icloud.com>
2020-06-26 14:58:32 -0600

    neos refactored and tested

--- near_earth_objects_test.rb
+++ near_earth_objects_test.rb
@@ -4,8 +4,36 @@ require 'pry'
 require_relative 'near_earth_objects'
 
 class NearEarthObjectsTest < Minitest::Test
+
+  def setup
+    @neos = NearEarthObjects.new('2019-03-30')
+  end
+
   def test_a_date_returns_a_list_of_neos
     results = NearEarthObjects.find_neos_by_date('2019-03-30')
     assert_equal '(2019 GD4)', results[:asteroid_list][0][:name]
   end
+
+  def test_it_has_attributes
+    assert_equal '2019-03-30', @neos.date
+  end
+
+  def test_neos_results
+    results = NearEarthObjects.find_neos_by_date('2019-03-30')
+    assert_equal Hash, results.class
+    assert_equal 3, results.length
+  end
+  # def test_largest_asteroid_diameter
+  #   require "pry"; binding.pry
+  #   @neos.formatted_asteroid_data
+  # end
+  #
+  # skip def test_formatted_asteroid_data
+  #   # code
+  # end
+  #
+  # skip def test_total_number_of_asteroids
+  #   # code
+  # end
+
 end
--- test/printer_test.rb
+++ test/printer_test.rb
@@ -5,6 +5,7 @@ require './lib/printer'
 class PrinterTest < Minitest::Test
   def setup
     @printer = Printer.new
+    @spacer = "________________________________________________________________________________________________________________________________"
   end
 
   def test_it_exists
@@ -12,14 +13,17 @@ class PrinterTest < Minitest::Test
   end
 
   def test_it_can_print_welcome_message
-    spacer = "________________________________________________________________________________________________________________________________"
     welcome = "Welcome to NEO. Here you will find information about how many meteors, astroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
     date_prompt = "Please enter a date in the following format YYYY-MM-DD."
     carrot_boys = ">>"
 
-    assert_equal spacer, @printer.spacer
+    assert_equal @spacer, @printer.spacer
     assert_equal welcome, @printer.welcome
     assert_equal date_prompt, @printer.date_prompt
     assert_equal carrot_boys, @printer.carrots
   end
+
+  # def test_it_can_print_results
+  #   # code
+  # end
 end
